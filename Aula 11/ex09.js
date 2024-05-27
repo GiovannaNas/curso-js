e24a4bfaa372b7f8661c7c5e7ada1daf19aa34e7
Giovanna Alves Do Nascimento <134456621+GiovannaNas@users.noreply.github.com>
2024-05-27 20:21:03 -0300

    ex 12
    ex 12

--- Aula 11/ex09.js
+++ Aula 11/ex09.js
@@ -1,1 +1,6 @@
-console.log (`O console funcionou corretamente!`)
+var vel = 50
+console.log(`A velocidade do seu carro e ${vel}Km/h`);
+if (vel> 60) { // condicao simples 
+    console.log (`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log (`Dirija sempre usando sinto de seguranca!`)
