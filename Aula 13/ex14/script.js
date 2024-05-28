d59fadcf645e81a6c111b099bff54dd025f79f3e
Giovanna Alves Do Nascimento <134456621+GiovannaNas@users.noreply.github.com>
2024-05-28 20:13:31 -0300

    up
    up

--- Aula 13/ex14/script.js
+++ Aula 13/ex14/script.js
@@ -1,1 +1,20 @@
-alert ('ola!')
+function carregar() {
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = 9  //data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas.`; 
+    if (hora >= 0 && hora < 12) {
+    //BOM DIA 
+img.src = 'assets/manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE
+        img.src = 'assets/tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // BOA NOITE
+        img.src = 'assets/noite.png'
+        document.body.style.background = '#515154'
+    }
+}
