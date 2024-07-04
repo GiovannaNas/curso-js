bc05464f49dd212170e7ee6c7a07fc96ada48aef
Giovanna Alves Do Nascimento <134456621+GiovannaNas@users.noreply.github.com>
2024-07-03 21:27:16 -0300

    organizacao de pastas
    organizacao de pastas

--- Aula 06/ex12.js
+++ Aula 06/ex12.js
@@ -1,6 +1,6 @@
 var agora = new Date()
 var hora = agora.getHours ()
-console.log (`Agora sao exatamente ${hora} horas.`)
+console.log (`Agora são exatamente ${hora} horas.`)
 if (hora <12) {
     console.log ('Bom dia!')
 } else if (hora <=18) {
--- Aula 11/ex09.js
+++
@@ -1,6 +0,0 @@
-var vel = 50
-console.log(`A velocidade do seu carro e ${vel}Km/h`);
-if (vel> 60) { // condicao simples 
-    console.log (`Voce ultrapassou a velocidade permitida. MULTADO!`)
-}
-console.log (`Dirija sempre usando sinto de seguranca!`)
